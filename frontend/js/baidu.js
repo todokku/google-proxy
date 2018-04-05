7e22ebd08da6f42d1bf9d8b24097221cfc1de54b
pn_paradise <2357782652@qq.com>
2018-04-05 09:49:22 +0000

    add baidu tongji

--- frontend/js/baidu.js
+++ frontend/js/baidu.js
@@ -1,5 +1,9 @@
-$(function(){
-    $.prettyform({
-        id: "login-form"
-    });
-});
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?04f41f59ebd6cba16f5dd06a88c28ee6";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
