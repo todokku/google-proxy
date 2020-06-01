5c45b84f6787b65a57a8f70fd0b597e7743db145
zhiqiang.huang <609888703@qq.com>
2020-06-01 18:31:00 +0800

    request promote

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var path = require('path');
-var express = require('express');
-var pkg = require( path.join(__dirname, 'package.json') );
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const path = require('path');
+const express = require('express');
+const pkg = require( path.join(__dirname, 'package.json') );
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var google = require('./lib/google');
-var request = require("request");
+const google = require('./lib/google');
+const https = require("https");
+const http = require('http');
+const url = require("url");
+const httpAgent = require('https-proxy-agent');
 
 let websiteList = require( path.join(__dirname, 'website.json') );
 
@@ -54,7 +57,7 @@ app.get('/search', function(req, res, next){
 	let time = new Date().toLocaleString();
 	console.info(time + " " + req.query.q);
 
-	google(req, res).search(req.query.q, req.query.page, req.query).then(function(body){
+	google(req, res).search().then(function(body){
 		res.render("home", body);
 	}).catch(function(error, resp, body){
 		res.send(body);
@@ -101,12 +104,45 @@ app.get('/setLanguage', function(req, res){
 
 
 app.get('/url', function(req, res){
-	res.redirect('https://r.5432.pro/-----' + req.query.q);
+	res.redirect('https://s.gaggga.workers.dev/-----' + req.query.q);
 	//req.pipe(request({rejectUnauthorized: false, url: encodeURI(req.query.q)})).pipe(res);
 });
 
-app.get('/ref', function(req, res){
-	req.pipe(request({rejectUnauthorized: false, url: encodeURI(req.query.q)})).pipe(res);
+app.get('/ref', function(request, response){
+	request.pause();
+	let options = url.parse(request.query.q);
+	options.method = request.method;
+	options.headers = {
+		'accept': '*/*',
+		'accept-language': 'zh-CN,zh;q=0.9,la;q=0.8',
+		'accept-encoding': 'gzip, deflate, br',
+		'cache-control': 'no-cache',
+		'host': options.host,
+		'sec-fetch-mode': "cors",
+		'sec-fetch-site': "cross-site",
+		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
+	};
+	options.agent = process.env.http_proxy && new httpAgent(process.env.http_proxy) || false;
+
+	let connector = null;
+	if(options.protocol === 'https:'){
+		connector = https.request(options, function(serverResponse) {
+			serverResponse.pause();
+			response.writeHeader(serverResponse.statusCode, serverResponse.headers);
+			serverResponse.pipe(response);
+			serverResponse.resume();
+		});
+	}else{
+		connector = http.request(options, function(serverResponse) {
+			serverResponse.pause();
+			response.writeHeader(serverResponse.statusCode, serverResponse.headers);
+			serverResponse.pipe(response);
+			serverResponse.resume();
+		});
+	}
+
+	request.pipe(connector);
+	request.resume();
 });
 
 // Everything is setup. Listen on the port.
