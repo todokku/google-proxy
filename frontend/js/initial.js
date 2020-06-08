97c773249727b945de51681296ca1380134c25d2
zhiqiang.huang <609888703@qq.com>
2020-06-08 18:07:04 +0800

    header style

--- frontend/js/initial.js
+++ frontend/js/initial.js
@@ -55,45 +55,58 @@
             $('.option-group').hide();
         });
 
-			$('select').each((index, item) => {
-			    item = $(item);
-				item.hide();
-				let value = item.val();
-				let selector = $('<div class="selector"></div>');
-				let label = $('<div class="label"></div>');
-                let dropdownArrow = $('<div class="dropdown-arrow"></div>');
-                selector.append(label);
-                selector.append(dropdownArrow);
-                let optionGroup = $('<div class="option-group"></div>');
-                selector.hover((e) =>{
-                    $('.option-group').not(optionGroup).hide();
-                    optionGroup.show();
-                });
+        $.fn.extend({
+			selector(options) {
+				$('select').each((index, item) => {
+					item = $(item);
+					item.hide();
+					let value = item.val();
+					let selector = $('<div class="selector"></div>');
+					let label = $('<div class="label"></div>');
+					let dropdownArrow = $('<div class="dropdown-arrow"></div>');
+					selector.append(label);
+					selector.append(dropdownArrow);
+					let optionGroup = $('<div class="option-group"></div>');
+					selector.hover((e) =>{
+						$('.option-group').not(optionGroup).hide();
+						optionGroup.show();
+					});
+					optionGroup.mouseleave(function(e){
+						e.stopPropagation();
+						optionGroup.hide();
+					});
 
-				optionGroup.hide();
-				item.find('option').each((idx, option) => {
-					let text = $(option).text();
-					let val = $(option).val();
+					optionGroup.hide();
+					item.find('option').each((idx, option) => {
+						let text = $(option).text();
+						let val = $(option).val();
 
-					let opt = $('<div class="option"></div>').text(text).data('val', val);
-                    opt.click((e) => {
-                        e.stopPropagation();
-                        label.text(text);
-                        item.val(val);
-                        selector.find('.option').not(opt).removeClass('selected');
-                        opt.addClass('selected');
-                        optionGroup.hide();
-                    });
-                    if(value === val){
-                        opt.click();
-                    }
-					opt.appendTo(optionGroup);
-				});
-				selector.append(optionGroup);
+						let opt = $('<div class="option"></div>').text(text).data('val', val);
+						opt.click((e) => {
+							e.stopPropagation();
+							label.text(text);
+							item.val(val);
+							selector.find('.option').not(opt).removeClass('selected');
+							opt.addClass('selected');
+							optionGroup.hide();
+							selector.onSelect(val, text);
+						});
+						if(value === val){
+							opt.click();
+						}
+						opt.appendTo(optionGroup);
+					});
+					selector.append(optionGroup);
 
-				selector.insertBefore(item);
-                selector.width(optionGroup.width());
-			})
+					selector.insertBefore(item);
+					selector.width(optionGroup.width());
+
+
+				})
+			}
+		});
+
+
 
 
 
