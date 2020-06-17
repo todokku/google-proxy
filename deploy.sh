75e331bfdc65ee0f3e12a8197f3b991c47cfc509
zhiqiang.huang <609888703@qq.com>
2020-06-17 10:23:02 +0800

    build slave

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,8 @@ git fetch --all
 git reset --hard origin/master
 npm install
 docker restart so
+until [ "`/usr/bin/docker inspect -f {{.State.Running}} so`" == "true" ]
+do
+    sleep 0.1
+done
+docker restart so_slave
